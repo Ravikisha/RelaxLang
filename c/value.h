120f136aaceb6ca65c54d9b18b0daa4893db6c33
ravikishan.69 <ravikishan63392@gmail.com>
2024-03-14 10:55:33 +0530

    Working on operators

--- c/value.h
+++ c/value.h
@@ -3,13 +3,35 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
 
 typedef struct {
   int capacity;
   int count;
   Value* values;
 } ValueArray;
+
+bool valuesEqual(Value a, Value b);
 
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
